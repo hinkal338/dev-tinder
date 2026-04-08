76addf503a51ed965c23e23f33c9804908dded10
Hinkal Jain <hinkaljain@Hinkals-MacBook-Air.local>
2026-04-09 01:08:39 +0530

    post and get apis

--- src/app.js
+++ src/app.js
@@ -4,15 +4,10 @@ const User = require('./models/user');
 
 const app = express();
 
-app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: "abc",
-        lastName: "d",
-        emailId: "abc.d@gmail.com",
-        password: "bsihaoijd",
-    };
-    const user = new User(userObj);
+app.use(express.json());
 
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User created successfully !!");
@@ -20,6 +15,34 @@ app.post('/signup', async (req, res) => {
         res.status(400).send("Error saving the user: " + err.message);
     }
 });
+
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch {
+        res.status(400).send("Something went wrong !!");
+    }
+});
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find();
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch {
+        res.status(400).send("Something went wrong !!");
+    }
+});
+
 connectDB().then(() => {
     console.log("Database connected successfully");
     app.listen(3000, () => {
